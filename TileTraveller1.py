bbfbe238f16ebabff31685985a90a6c0d1eb76c7
Matthias Finnur <matthias18@ru.is>
2018-09-18 11:45:20 +0000

    rett byrjadur

---
+++ TileTraveller1.py
@@ -0,0 +1,19 @@
+print("You can travel: ")
+breyting = str(input("Direction: "))
+stadsetning = 1
+north = False
+south = False
+west = False
+east = False
+
+while True:
+    if stadsetning == {1, 2, 4, 7, 8}:
+        north = True
+    elif stadsetning == {2, 3, 5, 8, 9}:
+        south = True
+    elif stadsetning == {5, 6, 9}: 
+        west = True
+    elif stadsetning == {2, 3, 6}:
+        east = True
+    
+
