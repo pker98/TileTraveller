4d3a668fe8710675b60133892b44a019bcaa52bb
Matthias Finnur <matthias18@ru.is>
2018-09-18 12:20:48 +0000

    Halfnad verkefni

--- TileTraveller1.py
+++ TileTraveller1.py
@@ -1,19 +1,42 @@
-print("You can travel: ")
-breyting = str(input("Direction: "))
-stadsetning = 1
-north = False
-south = False
-west = False
-east = False
 
-while True:
-    if stadsetning == {1, 2, 4, 7, 8}:
-        north = True
-    elif stadsetning == {2, 3, 5, 8, 9}:
-        south = True
-    elif stadsetning == {5, 6, 9}: 
-        west = True
-    elif stadsetning == {2, 3, 6}:
-        east = True
-    
+x = 1
+y = 1
+stadsetning = (x, y)
+rett_val = ""
 
+while stadsetning != 9:
+    if stadsetning == (1,1) or (2,1):
+        print("You can travel (N)orth")
+        breyting = str(input("Direction: ").lower())
+        rett_val = "n"
+    elif stadsetning == (1,2):
+        print("You can travel (N)orth or (S)outh or (W)est.")
+        breyting = str(input("Direction: ").lower())
+        rett_val = "n" or "s" or "w"
+    elif stadsetning == (2,2) or (3,3):
+        print("You can travel (S)outh or (W)est.")
+        breyting = str(input("Direction: ").lower())
+        rett_val = "s" or "w"
+    elif stadsetning == (1,3):
+        print("You can travel (S)outh or (E)ast.")
+        breyting = str(input("Direction: ").lower())
+        rett_val = "s" or "e"
+    elif stadsetning == (2,3):
+        print("You can travel (E)ast or (W)est.")
+        breyting = str(input("Direction: ").lower())
+        rett_val = "e" or "w"
+    elif stadsetning == (3,2):
+        print("You can travel (N)orth or (S)outh.")
+        breyting = str(input("Direction: ").lower())
+        rett_val = "n" or "s"
+
+    if breyting == rett_val:
+        if breyting == "n":
+            y += 1
+        elif breyting == "s":
+            y -= 1
+        elif breyting == "w":
+            x -= 1
+        elif breakpoint == "e":
+            x += 1
+            
