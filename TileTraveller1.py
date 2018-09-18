a16d6465ca35d25172175096391f9227b4d43a07
Matthias Finnur <matthias18@ru.is>
2018-09-18 13:53:57 +0000

    næstum klarad

--- TileTraveller1.py
+++ TileTraveller1.py
@@ -1,53 +1,34 @@
 
 stadsetning = 1
-rett_val = ""
 
 while stadsetning != 7:
     if stadsetning == 1 or stadsetning == 4:
-        print("You can travel: (N)orth")
-        breyting = str(input("Direction: ").lower())
-        if breyting != "n":
-            print("Not a valid direction")
-            continue
+        print("You can travel: (N)orth.")     
     elif stadsetning == 2:
-        print("You can travel: (N)orth or (S)outh or (E)ast.")
-        breyting = str(input("Direction: ").lower())
-        if breyting != "n" and breyting != "s" and breyting != "e":
-            print("Not a valid direction")
-            continue
-    elif stadsetning == 5 or stadsetning == 9:
+        print("You can travel: (N)orth or (E)ast or (S)outh.")
+    elif (stadsetning == 5 or stadsetning == 9):
         print("You can travel: (S)outh or (W)est.")
-        breyting = str(input("Direction: ").lower())
-        if breyting != "s" and breyting != "w":
-            print("Not a valid direction")
-            continue
     elif stadsetning == 3:
-        print("You can travel: (S)outh or (E)ast.")
-        breyting = str(input("Direction: ").lower())
-        if breyting != "s" and breyting != "e":
-            print("Not a valid direction")
-            continue
+        print("You can travel: (E)ast or (S)outh.")
     elif stadsetning == 6:
         print("You can travel: (E)ast or (W)est.")
-        breyting = str(input("Direction: ").lower())
-        if breyting != "e" and breyting != "w":
-            print("Not a valid direction")
-            continue
     elif stadsetning == 8:
         print("You can travel: (N)orth or (S)outh.")
-        breyting = str(input("Direction: ").lower())
-        if breyting != "n" and breyting != "s":
-            print("Not a valid direction")
-            continue
 
-    if breyting == "n":
+    breyting = str(input("Direction: ").lower())
+    
+    if breyting == "n" and (stadsetning == 1 or 4 or 2 or 8):
         stadsetning += 1
-    elif breyting == "s":
+    elif breyting == "s" and (stadsetning == 2 or 5 or 9 or 3 or 8):
         stadsetning -= 1
-    elif breyting == "w":
+    elif breyting == "w" and (stadsetning == 5 or 9 or 6):
         stadsetning -= 3
-    elif breyting == "e":
+    elif breyting == "e" and (stadsetning == 2 or 3 or 6):
         stadsetning += 3
+    else: 
+        print("Not a valid direction!")
+    
 else:
     print("Victory!")
+    exit()
             
