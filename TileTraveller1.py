4181a0f88e91ee597252c441818b24efbc8eada4
Matthias Finnur <matthias18@ru.is>
2018-09-18 12:42:27 +0000

    verkefni klarad en ljotur kodi

--- TileTraveller1.py
+++ TileTraveller1.py
@@ -1,42 +1,53 @@
 
-x = 1
-y = 1
-stadsetning = (x, y)
+stadsetning = 1
 rett_val = ""
 
-while stadsetning != 9:
-    if stadsetning == (1,1) or (2,1):
-        print("You can travel (N)orth")
+while stadsetning != 7:
+    if stadsetning == 1 or stadsetning == 4:
+        print("You can travel: (N)orth")
         breyting = str(input("Direction: ").lower())
-        rett_val = "n"
-    elif stadsetning == (1,2):
-        print("You can travel (N)orth or (S)outh or (W)est.")
+        if breyting != "n":
+            print("Not a valid direction")
+            continue
+    elif stadsetning == 2:
+        print("You can travel: (N)orth or (S)outh or (E)ast.")
         breyting = str(input("Direction: ").lower())
-        rett_val = "n" or "s" or "w"
-    elif stadsetning == (2,2) or (3,3):
-        print("You can travel (S)outh or (W)est.")
+        if breyting != "n" and breyting != "s" and breyting != "e":
+            print("Not a valid direction")
+            continue
+    elif stadsetning == 5 or stadsetning == 9:
+        print("You can travel: (S)outh or (W)est.")
         breyting = str(input("Direction: ").lower())
-        rett_val = "s" or "w"
-    elif stadsetning == (1,3):
-        print("You can travel (S)outh or (E)ast.")
+        if breyting != "s" and breyting != "w":
+            print("Not a valid direction")
+            continue
+    elif stadsetning == 3:
+        print("You can travel: (S)outh or (E)ast.")
         breyting = str(input("Direction: ").lower())
-        rett_val = "s" or "e"
-    elif stadsetning == (2,3):
-        print("You can travel (E)ast or (W)est.")
+        if breyting != "s" and breyting != "e":
+            print("Not a valid direction")
+            continue
+    elif stadsetning == 6:
+        print("You can travel: (E)ast or (W)est.")
         breyting = str(input("Direction: ").lower())
-        rett_val = "e" or "w"
-    elif stadsetning == (3,2):
-        print("You can travel (N)orth or (S)outh.")
+        if breyting != "e" and breyting != "w":
+            print("Not a valid direction")
+            continue
+    elif stadsetning == 8:
+        print("You can travel: (N)orth or (S)outh.")
         breyting = str(input("Direction: ").lower())
-        rett_val = "n" or "s"
+        if breyting != "n" and breyting != "s":
+            print("Not a valid direction")
+            continue
 
-    if breyting == rett_val:
-        if breyting == "n":
-            y += 1
-        elif breyting == "s":
-            y -= 1
-        elif breyting == "w":
-            x -= 1
-        elif breakpoint == "e":
-            x += 1
+    if breyting == "n":
+        stadsetning += 1
+    elif breyting == "s":
+        stadsetning -= 1
+    elif breyting == "w":
+        stadsetning -= 3
+    elif breyting == "e":
+        stadsetning += 3
+else:
+    print("Victory!")
             
